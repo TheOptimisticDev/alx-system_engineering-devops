54d0f17857b5eacf75acd33f240bac875fcb83b2
OptimisticUnderdog <mabunda.wealth@gmail.com>
2023-03-16 00:57:48 -0700

    commit

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,2 @@
 !/usr/bin/env ruby
-# Script outputs: [SENDER], [RECEIVER], [FLAGS]
-# Sender phone number or name
-# receiver phone number or name
-# flags used
-
-puts ARGV[0].scan(/(?<=from:|to:|flags:)[^\]]*/).join(',')
+puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
